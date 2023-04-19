338c1f10a12125a71f8056cea0a880e8727a13a6
zeeshanriasat302 <zeeshan.riasat302@gmail.com>
2023-04-19 16:35:05 +0500

    challenge ping tree test task completed, decisions will be made based on visitor information, no target can recieve more traffic per day tha it allows

---
+++ index.js
@@ -0,0 +1,8 @@
+var name = require('./package.json').name
+require('productionize')(name)
+
+var server = require('./lib/server')
+
+var port = process.env.PORT || 5000
+server.listen(port)
+console.log(name, 'listening on port', port)
